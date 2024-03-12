3839e6ffdc46d384c6f4c5f655b872ba18160b79
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-03-12 09:48:34 -0300

    proxy finished

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+
 docker build -t repo:proxy -f proxy/Dockerfile .
 docker build -t repo:app1  -f app1/Dockerfile .
 docker build -t repo:app2  -f app2/Dockerfile .
 
-#docker compose up
+docker compose up
